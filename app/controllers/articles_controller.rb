2f1508a79cba7271285eb3eeb23f2da42ce430c0
Alex Smirnov <sas2job@gmail.com>
2019-04-29 13:51:56 +0300

    Articles update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ def new
   @article = Article.new
 end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
 def create
   @article = Article.new(article_params)
 
@@ -19,6 +23,16 @@ def create
     redirect_to @article
   else
     render 'new'
+  end
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
   end
 end
 
